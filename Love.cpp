d50e7afb4c72c48ddc653b98d8ca88bbb5014326
gelite02 <140132886+gelite02@users.noreply.github.com>
2023-08-01 23:22:15 +0800

    Add files via upload

---
+++ Love.cpp
@@ -0,0 +1,16 @@
+#include "Love.h"
+#include<stdio.h>
+int main()
+{
+	char character1,character2,character3,character4;
+	character1='L';
+	character2='o';
+	character3='v';
+	character4='e';
+	putchar(character1);
+	putchar(character2);
+	putchar(character3);
+	putchar(character4);
+	putchar('\n');
+	return 0;
+}

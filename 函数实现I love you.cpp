12a3a2b41e8c28999428dc22561d6ed6eb55d912
gelite02 <140132886+gelite02@users.noreply.github.com>
2023-08-04 20:39:44 +0800

    Add files via upload

---
+++ 函数实现I love you.cpp
@@ -0,0 +1,21 @@
+#include "º¯ÊıÊµÏÖI love you.h"
+#include<stdio.h>
+int main()
+{
+	int special_Symbols();
+	int text();
+	special_Symbols();
+	text();
+	special_Symbols();
+	return 0;
+}
+int special_Symbols()
+{
+	printf("***********\n");
+	return 0;
+}
+int text()
+{
+	printf("I love you!\n");
+	return 0;
+}
